c2429d11b87cfb92cc72cb519f3a5cab580e4984
Benno Zeeman <bzeeman@live.nl>
2025-09-17 10:46:28 +0200

    chore: change path settings SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,10 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({ fallback: '404.html' }) },
+	paths: {
+		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+	},
 	extensions: ['.svelte', '.md']
 };
 
